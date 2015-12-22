dbf640256e76108be7c2893187a770b6f8aa25b4
mimman <mimman1@naver.com>
2015-12-22 16:25:34 +0900

    mybatis 추가 수정 제 검색

--- src/main/java/com/mimman/board/service/BoardServiceImpl.java
+++ src/main/java/com/mimman/board/service/BoardServiceImpl.java
@@ -1,16 +1,14 @@
 package com.mimman.board.service;
 
-import java.sql.PreparedStatement;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.jdbc.core.RowMapper;
-
 import com.mimman.board.repository.BoardDto;
+
+import mybatis.BoardManager;
 
 public class BoardServiceImpl implements BoardService {
 	private JdbcTemplate jdbcTemplate;
@@ -20,12 +18,15 @@ public class BoardServiceImpl implements BoardService {
 	}
 
 	public List getList() throws SQLException {
+		/*
 		String sql = "select * from tblSpringBoard order by b_seq desc";
 		List<BoardDto> list = new ArrayList<BoardDto>();
 
 		list = jdbcTemplate.query(sql, new RowMapperImpl());
 
 		return list;
+		*/
+		return BoardManager.getList();
 	}
 
 	class RowMapperImpl implements RowMapper {
@@ -44,13 +45,20 @@ public class BoardServiceImpl implements BoardService {
 
 	}
 
-	public BoardDto findBySeq(int seq) throws SQLException {
+	public BoardDto findBySeq(int b_seq) throws SQLException {
+		/*
 		String sql = "select * from tblSpringBoard where b_seq=" + seq;
 		BoardDto dto = jdbcTemplate.queryForObject(sql, new RowMapperImpl());
 		return dto;
+		*/
+		return BoardManager.findBySeq(b_seq);
 	}
 
 	public void write(BoardDto dto) throws SQLException {
+		BoardManager.write(dto);
+
+		
+		/*
 		String sql = "insert into tblSpringBoard(b_seq,b_title,b_content,b_writer,"
 				+ "b_regdate,b_hitcount,b_password) values(seq_board.nextVal,?,?,?,sysdate,0,?)";
 
@@ -61,7 +69,9 @@ public class BoardServiceImpl implements BoardService {
 
 		Object[] values = new Object[] { dto.getB_title(), dto.getB_content(), dto.getB_writer(), dto.getB_password() };
 		jdbcTemplate.update(sql, values);
+		*/
 	}
+	
 	/*
 	 * class WriteParams implements PreparedStatementSetter{ private BoardDto
 	 * dto;
@@ -77,20 +87,27 @@ public class BoardServiceImpl implements BoardService {
 	 */
 
 	public void update(BoardDto dto) throws SQLException {
+		BoardManager.update(dto);
+		/*
 		String sql = "update tblSpringBoard set b_title=?, b_content=? " + "where b_seq=?";
-		System.out.println("update" +"글번호"+ dto.getB_seq()+"제목"+dto.getB_title()+"내용"+dto.getB_content());
+		System.out.println("update" +"글번호"+ dto.getB_seq()+"제목"+dto.getB_title()+"내용"+dto.getB_content()+dto.getB_seq());
 		
 		Object[] val = new Object[] { dto.getB_title(), dto.getB_content(), dto.getB_seq() };
 		System.out.println(sql);
 		jdbcTemplate.update(sql, val);
+		*/
+		
 		
 	}
 
-	public void delete(int seq) throws SQLException {
+	public void delete(int b_seq) throws SQLException {
+		BoardManager.delete(b_seq);
+		/*
 		String sql = "delete from tblSpringBoard where b_seq=?";
 
 		Object[] val = new Object[] { seq };
 		jdbcTemplate.update(sql, val);
+		*/
 
 	}
 
